eaecb7e10572cfc5934645cb0e2bab3ca8452fc9
brozeph <joshua.thomas@gmail.com>
2013-09-06 12:05:19 -0700

    splitting functional from unit tests

---
+++ test/functional/index.js
@@ -0,0 +1,86 @@
+var
+	ascentis = requireWithCoverage('index'),
+
+	account = 'playnetwork',
+	clientKey = 'M65ipU/X/Lk+zt1nHDVlV0',
+	host = 'selfservice2.ascentis.com',
+	secretKey = 'sPJ7s1L2VQkYmTS4ZmiSpY';
+
+
+describe('functional tests for Ascentis API', function () {
+	'use strict';
+
+	this.timeout(25000);
+
+	var client;
+
+	beforeEach(function () {
+		client = ascentis.initialize({
+				account : account,
+				clientKey : clientKey,
+				host : host,
+				secretKey : secretKey
+			});
+
+		/*
+		client.request.on('request', function (options) {
+			console.log(options);
+		});
+		//*/
+	});
+
+	describe('#getBulkData', function () {
+		it('should properly retrieve bulk employee data', function (done) {
+			client.getBulkData(function (err, data) {
+				should.not.exist(err);
+				should.exist(data);
+
+				done();
+			});
+		});
+	});
+
+	describe('#getEmployees', function () {
+		it('should properly retrieve employees', function (done) {
+			client.getEmployees(function (err, data) {
+				should.not.exist(err);
+				should.exist(data);
+
+				done();
+			});
+		});
+	});
+
+	describe('#getFields', function () {
+		it('should properly retrieve custom defined fields', function (done) {
+			client.getFields(function (err, data) {
+				should.not.exist(err);
+				should.exist(data);
+
+				done();
+			});
+		});
+	});
+
+	describe('#getLocations', function () {
+		it('should properly retrieve all locations', function (done) {
+			client.getLocations(function (err, data) {
+				should.not.exist(err);
+				should.exist(data);
+
+				done();
+			});
+		});
+	});
+
+	describe('#getJobs', function () {
+		it('should properly retrieve all jobs defined', function (done) {
+			client.getJobs(function (err, data) {
+				should.not.exist(err);
+				should.exist(data);
+
+				done();
+			});
+		});
+	});
+});
