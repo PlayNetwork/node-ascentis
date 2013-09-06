3c829d3aacee39d674f750adfc87b5ae5af29be0
Joshua Thomas <joshua.thomas@gmail.com>
2013-09-05 19:26:04 -0700

    working out creation of headers for authorization on each request

--- test/lib/index.js
+++ test/lib/index.js
@@ -3,7 +3,7 @@ var
 
 	account = 'playnetwork',
 	clientKey = 'M65ipU/X/Lk+zt1nHDVlV0',
-	host = 'https://selfservice2.ascentis.com',
+	host = 'selfservice2.ascentis.com',
 	secretKey = 'sPJ7s1L2VQkYmTS4ZmiSpY';
 
 
@@ -21,15 +21,21 @@ describe('ascentis', function () {
 			});
 	});
 
-	describe('#getAuthHeader', function () {
+	describe('#getRequestHeaders', function () {
 		it('should properly build authentication header', function () {
-			var signature = client.getAuthHeader('/api/v1.1/employees');
-			should.exist(signature);
+			var headers = client.getRequestHeaders({ path : '/api/v1.1/employees' });
+			should.exist(headers);
+			should.exist(headers.Timestamp);
+			should.exist(headers.Authorization);
 		});
 	});
 
 	describe('#getEmployees', function () {
 		it('should properly retrieve employees', function (done) {
+			client.request.on('request', function (options) {
+				console.log(options);
+			});
+
 			client.getEmployees(function (err, employees) {
 				should.not.exist(err);
 				should.exist(employees);
